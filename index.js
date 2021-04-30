9ade897473c4193c1f18a5b4d21d39c8df52c05b
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:27:57 +0300

    lambda one line also

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function wordLengthClassifier(word) {
     return `I can handle also words in any length`
 }
 
-const newWorldLengthClassifier = word => {
-    return word.length > 7? `That's a long word!`: `I can handle also words in any length`
-}
+const newWorldLengthClassifier = word => word.length > 7? `That's a long word!`: `I can handle also words in any length`
 
 console.log(newWorldLengthClassifier('bananas'))
 console.log(newWorldLengthClassifier('bananas!'))
