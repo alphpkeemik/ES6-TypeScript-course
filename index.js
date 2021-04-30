fd58c20d03ab29577fc25837d57d289e830c3576
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 13:56:43 +0300

    await with error and catch

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const mockRequest = new Promise((resolve, reject) => {
     setTimeout(() => {
-        //reject(new Error('Oh no, something went wrong'))
+        reject(new Error('Oh no, something went wrong'))
         resolve ('foo')
     }, 1500)
 
 });
 (async () => {
-    const response = await mockRequest.then(value => value.split('').reverse().join(''))
-    console.log({response})
+    try {
+        const response = await mockRequest.then(value => value.split('').reverse().join(''))
+        console.log({response})
+    }catch(e) {
+        console.log('caught an error!')
+    }
+
+
 })()
