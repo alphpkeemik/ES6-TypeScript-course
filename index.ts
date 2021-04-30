f2ebfd41b8cef7d71b344639baa303fb24f6d1c7
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 15:28:34 +0300

    typing identity

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-function stringLogger(arg: any) {
+function logger<Type>(arg: Type):Type {
     console.log(arg)
     return arg
 }
+const stringLogger = (arg:string) => logger<string>(arg)
+const intLogger = (arg:number) => logger<number>(arg)
 
-const fooIdentity: string = stringLogger('foo')
+const fooIdentity = logger('foo')
+stringLogger(fooIdentity)
+const intIdentity = logger(1)
+intLogger(intIdentity)
