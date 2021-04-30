715d44cb3c83103516aae7be09c346a874abc1fb
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 12:17:34 +0300

    array map and nice template literals and destruct example

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
 const orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries', 'bananas', 'apples', 'strawberries']
 
 
-const logFruitsThatAreNotFavorite = (fruits) => {
+const capitalizeFruits = (fruits) => orderedFavoriteFruits.map((value, index, array) => {
+    const [first, ...characters] = value.split("")
 
-    const [...otherFruits] = fruits
-    console.log(otherFruits)
-    //console.log(`My second favorite fruit is ${secondFavorite}`)
-}
-//logFruitsThatAreNotFavorite(orderedFavoriteFruits)
-const personTemplate= {
-    name: 'Name not given',
-    email: 'E-mail not given',
-    password: '*****',
-    id: -1,
-    guid: 'aaaaaa-aaaaa-aaaaa-aaaa'
-}
+    return `${index+1}: ${first.toUpperCase()}${characters.join('')}`
+})
 
-const personMask= {
-    password: '*****',
-    id: -1,
-}
-const person= {
-    name: 'John',
-    email: 'smith@example.com',
-    password: '123pass',
-    id: 97,
-    guid: 'aaa-bbbb-123123123-ccc'
-}
-
-
-const personAddress = {
-    id : 56,
-    street: 'Ravi',
-    house: '2',
-    postalCode: '10203',
-}
-
-const combinePersonAndAddress = (person, personAddress) => {
-    return {
-        ...person,
-        ...personAddress
-    }
-}
-//console.log(combinePersonAndAddress(person, personAddress))
-console.log({...personTemplate, ...person, ...personMask})
+console.log(capitalizeFruits(orderedFavoriteFruits))
