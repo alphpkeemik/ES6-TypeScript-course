2b0e24b885e2a0223e79d54f09bf3d1029ba9845
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 11:00:43 +0300

    array destruct

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const  orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries']
 
 const logFavoriteFruit = (fruits) => {
-    const favorite = fruits[0]
-    const secondFavorite = fruits[1]
+   /* const favorite = fruits[0]
+    const secondFavorite = fruits[1]*/
+    const  [favorite, secondFavorite] = fruits
     console.log(`My favorite fruit is ${favorite} and second favorite is ${secondFavorite}`)
 }
 
