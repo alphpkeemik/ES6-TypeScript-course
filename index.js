a8d0a36486f27e4759a3b305e6a98589062b1ff0
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:28:35 +0300

    lambda one line does not work object

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function wordLengthClassifier(word) {
     return `I can handle also words in any length`
 }
 
-const newWorldLengthClassifier = word => word.length > 7? `That's a long word!`: `I can handle also words in any length`
+const newWorldLengthClassifier = word => {inputWord: word}
 
 console.log(newWorldLengthClassifier('bananas'))
 console.log(newWorldLengthClassifier('bananas!'))
