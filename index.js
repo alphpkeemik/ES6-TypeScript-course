3b50f3f926a9ddc13192e3e727bb21ef29a0fe2c
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:42:10 +0300

    car spoiler example metadata key function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const prefixWithMeta = key => {
+    return `meta:${key}`
+}
+// single line
+//const prefixWithMeta = key => `meta:${key}`
+
 const createCarObject = (make, model, year, metaKey, metaValue) => {
     return {
         make,
         model,
         year: year - 2,
         metadata: {
-            [`meta:${metaKey}`]: metaValue
+            [prefixWithMeta(metaKey)]: metaValue
         }
     }
 }
