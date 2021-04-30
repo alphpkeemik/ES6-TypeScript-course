ef6e2d6180cb3f17e2b331b6d77c4aedbd69b367
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:25:12 +0300

    simple function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const foo = 'foo'
-const bar = 'bar'
-const foobar = `${
-    foo + (foo.length)
-}:${
-    bar
-}`
+function wordLengthClassifier(word) {
+    if (word.length > 7) {
+        return `That's a long word!`
+    }
+    return `I can handle also words in any length`
+}
 
-
-console.log(foobar)
+console.log(wordLengthClassifier('bananas'))
+console.log(wordLengthClassifier('bananas!'))
