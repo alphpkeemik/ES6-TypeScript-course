c66e69fc85f1f5209cfc53c572a67a8f8a979381
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 13:50:31 +0300

    fetch

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const mockRequest = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        //reject(new Error('Oh no, something went wrong'))
-        resolve ('foo')
-    }, 1500)
-
-})
-const logger = arg => console.log(arg) || arg
-mockRequest
-
-    .then(arg => `bar and ${arg}`)
-    .then(logger)
-    .then(arg => console.log(`I went trough the entire promise chain and all I got was ${arg}`) )
-    .catch(console.error)
-    .finally(() => console.log(`finally`))
+// do in browser console!
+fetch('https://www.metaweather.com/api/location/search/?query=San')
+    .then(response => response.json())
+    .then(console.log)
