62b2df0615c2e281701299f887c2e94eabee9dcb
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 14:43:40 +0300

    ts union type and interface

--- index.ts
+++ index.ts
@@ -8,6 +8,10 @@ interface CarFactory {
     (make: string, model: string, year: number): Car
 }
 
+type Vehicle = {
+    pedalCount: number
+}
+
 const carFactory: CarFactory = (make: string, model: string, year: number): Car => {
     return {
         make,
@@ -28,9 +32,11 @@ const car1 = carFactory('Form', 'focus', 2006)
 const car2 = myCarFactory('Form', 'focus', 2006)
 console.log(car1, car2)
 
-const getCarInfo = (car: Car) => {
-    const {make, model, year} = car
+const printVehicleInfo = (vehicle: Car & Vehicle) => {
+    const {make, model, year} = vehicle
     return `This ${make} ${model} was manufactured in ${year}`
 }
 
-console.log(getCarInfo(car1))
+console.log(printVehicleInfo({
+    model: 'model'
+})
