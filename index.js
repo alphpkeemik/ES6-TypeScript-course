c9b22e778c7050f15129e15d778a5eb78269c750
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 14:35:16 +0300

    ts dont allow any default

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello me');
+var carFactory = function (make, model, year) {
+    return {
+        make: make,
+        model: model,
+        year: year
+    };
+};
+var car1 = carFactory('Form', 'focus', 2006);
+//console.log(car1)
+var getCarInfo = function (car) {
+    var make = car.make, model = car.model, year = car.year;
+    return "This " + make + " " + model + " was manufactured in " + year;
+};
+console.log(getCarInfo(car1));
--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ const carFactory =  (make :string, model: string, year: number) => {
 }
 
 const car1 = carFactory('Form', 'focus', 2006)
-console.log(car1)
-const car2 = carFactory(2006, false, 'year')
-console.log(car2)
+//console.log(car1)
+
+const getCarInfo = (car:any) => {
+    const {make, model, year} = car
+    return `This ${make} ${model} was manufactured in ${year}`
+}
+
+console.log(getCarInfo(car1))
