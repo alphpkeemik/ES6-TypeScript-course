6cb8c8cfb1bcf6817939f7a63fd8364cdfa35370
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 11:23:05 +0300

    array destruct empty var assignement

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-/*
-const  orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries']
+const orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries']
 
-const logFavoriteFruit = (fruits) => {
-   /!* const favorite = fruits[0]
-    const secondFavorite = fruits[1]*!/
-    const  [favorite, secondFavorite, third, fourth] = fruits
-    console.log(`My favorite fruit is ${favorite} and second favorite is ${secondFavorite}, there is no ${fourth}`)
+
+const logSecondFavoriteFruit = (fruits) => {
+
+    const [, secondFavorite] = fruits
+    console.log(`My second favorite fruit is ${secondFavorite}`)
 }
-
-logFavoriteFruit(orderedFavoriteFruits)*/
-
-
-const person = {
-    name: 'John',
-    email: 'john@smith.example.com',
-    password: 'imtheman'
-}
-
-let defaultPerson = {name: 'no name provided', email: 'no email provided'};
-const personLogger = (person= defaultPerson) => {
-    const {name, email} = Object.assign(defaultPerson, person)
-    console.log(`${name}, ${email}`)
-}
-
-personLogger({name: 'John'})
+logSecondFavoriteFruit(orderedFavoriteFruits)
