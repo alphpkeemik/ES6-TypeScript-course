ee0f18b00fa4e19daf07c2bd8f929de34da2af43
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 11:01:37 +0300

    array destruct some missing

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const  orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries']
 const logFavoriteFruit = (fruits) => {
    /* const favorite = fruits[0]
     const secondFavorite = fruits[1]*/
-    const  [favorite, secondFavorite] = fruits
-    console.log(`My favorite fruit is ${favorite} and second favorite is ${secondFavorite}`)
+    const  [favorite, secondFavorite, third, fourth] = fruits
+    console.log(`My favorite fruit is ${favorite} and second favorite is ${secondFavorite}, there is no ${fourth}`)
 }
 
 logFavoriteFruit(orderedFavoriteFruits)
