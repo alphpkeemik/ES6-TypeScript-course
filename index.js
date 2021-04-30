620cadc5d997728eea16e5397dcea61d90f45e35
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 13:43:33 +0300

    promise catch must be at end

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const mockRequest = new Promise((resolve, reject) => {
     setTimeout(() => {
-        //reject(new Error('Oh no, something went wrong'))
+        reject(new Error('Oh no, something went wrong'))
         resolve ('foo')
     }, 1500)
 
 })
 const logger = arg => console.log(arg) || arg
 mockRequest
-    .catch(console.error)
+
     .then(arg => `bar and ${arg}`)
     .then(logger)
     .then(arg => console.log(`I went trough the entire promise chain and all I got was ${arg}`))
+    .catch(console.error)
