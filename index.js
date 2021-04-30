4c587d53e7563beeafd8d6305554480d83aad0e4
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 12:45:19 +0300

    array every

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ const people = [
 
 
 const detectUnderAgePerson = ({age}, index, array) => {
-    return age<18
+    return age < 18
 }
 
 const underAgePerson = people.find((value, index, array) => detectUnderAgePerson(value, index, array))
 // or const underAgePerson = people.find((value, index, array) => detectUnderAgePerson(value))
 // or const underAgePerson = people.find(detectUnderAgePerson)
 
-console.log(underAgePerson)
+const everyOneIsOfAge = people.every(({age}) => age >= 18)
+
+console.log(everyOneIsOfAge)
+
+
