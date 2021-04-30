cb7129725f524d2a5eb241e3c630c4f2cac47685
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:50:22 +0300

    custom logging function

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const prefixWithMeta = key => {
 // single line
 //const prefixWithMeta = key => `meta:${key}`
 
-const createCarObject = (make, model = 'focus', year = 2000) => {
+const createCarObject = (make, model = 'focus', year = 2000, loggingFn1 = () => {
+    console.log(`${make} ${model} from ${year}`)
+}) => {
     return {
         make,
         model,
         year: year - 2,
-        logData: () => {
-            console.log(`${make} ${model} from ${year}`)
-        }
+        logData: loggingFn1
     }
 }
 
 let car = createCarObject('Ford', 'Ka', 2009);
 car.logData()
+let tesla = createCarObject('Tesla', 'cybertruck', 2020, () => {
+    console.log('Elon made this')
+});
+tesla.logData()
