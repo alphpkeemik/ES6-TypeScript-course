ca58b5292a52eb5dda9bc5e93a0dd849112daef2
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 12:57:10 +0300

    dedupe method

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 
 const orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries', 'bananas', 'apples', 'strawberries', 'avocado', 'apples']
+const dedupe = array => Array.from(new Set(array))
 console.log([...new Set(orderedFavoriteFruits)])
+
+console.log(dedupe(orderedFavoriteFruits))
