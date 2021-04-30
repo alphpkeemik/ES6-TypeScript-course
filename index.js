2497731e3282f2e1cbbefae3161525607cbf8450
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 14:45:36 +0300

    ts or type and interface

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ var carFactory = function (make, model, year) {
         year: year
     };
 };
+var myCarFactory = function (make, model, year) {
+    return {
+        make: make,
+        model: model,
+        year: year + 1
+    };
+};
 var car1 = carFactory('Form', 'focus', 2006);
-//console.log(car1)
-var getCarInfo = function (car) {
-    var make = car.make, model = car.model, year = car.year;
+var car2 = myCarFactory('Form', 'focus', 2006);
+console.log(car1, car2);
+var printVehicleInfo = function (vehicle) {
+    var make = vehicle.make, model = vehicle.model, year = vehicle.year;
     return "This " + make + " " + model + " was manufactured in " + year;
 };
-console.log(getCarInfo(car1));
+console.log(printVehicleInfo({
+    model: 'model'
+}));
--- index.ts
+++ index.ts
@@ -32,11 +32,8 @@ const car1 = carFactory('Form', 'focus', 2006)
 const car2 = myCarFactory('Form', 'focus', 2006)
 console.log(car1, car2)
 
-const printVehicleInfo = (vehicle: Car & Vehicle) => {
+const printVehicleInfo = (vehicle: Car | Vehicle) => {
     const {make, model, year} = vehicle
     return `This ${make} ${model} was manufactured in ${year}`
 }
 
-console.log(printVehicleInfo({
-    model: 'model'
-})
