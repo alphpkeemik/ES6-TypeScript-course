ceafb77891a875f0b3320ee23c9502896861a54e
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 11:03:46 +0300

    object var caccess

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+/*
 const  orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries']
 
 const logFavoriteFruit = (fruits) => {
-   /* const favorite = fruits[0]
-    const secondFavorite = fruits[1]*/
+   /!* const favorite = fruits[0]
+    const secondFavorite = fruits[1]*!/
     const  [favorite, secondFavorite, third, fourth] = fruits
     console.log(`My favorite fruit is ${favorite} and second favorite is ${secondFavorite}, there is no ${fourth}`)
 }
 
-logFavoriteFruit(orderedFavoriteFruits)
+logFavoriteFruit(orderedFavoriteFruits)*/
+
+
+const fordFocus = {
+    make: 'ford',
+    model: 'focus',
+    year: 2009
+}
+
+const logCarMake = car => {
+    const make = car.make
+    console.log(`Car make ${make}`)
+}
+
+logCarMake(fordFocus)
