03f085433aaf275f2ad372f6a054078afe01e029
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 12:53:41 +0300

    set as unique entries

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const people = [
-    {
-        name: 'John',
-        email: 'john@example.com',
-        age: 17,
-    },
-    {
-        name: 'Jane',
-        email: 'jane@.example.com',
-        age: 29,
-    },
-    {
-        name: 'Juss',
-        email: 'juss@example.com',
-        age: 63,
-    },
-]
-const [person] = people
 
-console.log(Object.keys(person))
-console.log(Object.values(person))
-console.log(Object.entries(person))
+const orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries', 'bananas', 'apples', 'strawberries', 'avocado', 'apples']
+console.log(new Set(orderedFavoriteFruits))
