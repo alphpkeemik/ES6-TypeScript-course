264a7052cd64dafaea9a3b1350c1c86be85acfc3
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:30:25 +0300

    complex lamda ternary

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function wordLengthClassifier(word) {
-    if (word.length > 7) {
-        return `That's a long word!`
-    }
-    return `I can handle also words in any length`
-}
-
-const newWorldLengthClassifier = word => [word]
+const newWorldLengthClassifier = word => word.length > 7 && `that's a long word`
 
 console.log(newWorldLengthClassifier('bananas'))
 console.log(newWorldLengthClassifier('bananas!'))
