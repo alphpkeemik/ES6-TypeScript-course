5950d7630e831b99b2973707e8c59c905dd109ce
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 13:38:16 +0300

    promise then error

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mockRequest = new Promise((resolve, reject) => {
     setTimeout(() => {
+        reject(new Error('Oh no, something went wrong'))
         resolve ('foo')
     }, 1500)
 
 })
 
-mockRequest.then(console.log)
+mockRequest
+    .then(console.log)
