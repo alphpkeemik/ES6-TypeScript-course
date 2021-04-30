352edefa96eddc953fff1965b52923b608ac07bd
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 10:54:00 +0300

    custom logging function arguments

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const createCarObject = (make, model = 'focus', year = 2000, loggingFn1 = loggin
         make,
         model,
         year: year - 2,
-        logData: loggingFn1(make, model, year)
+        logData: () => loggingFn1(make, model, year)
     }
 }
 
 let car = createCarObject('Ford', 'Ka', 2009);
 car.logData()
-let tesla = createCarObject('Tesla', 'cybertruck', 2020, () => {
-    console.log('Elon made this')
+let tesla = createCarObject('Tesla', 'cybertruck', 2020, (make, model, year) => {
+    console.log(`Elon made ${make} ${model} at ${year}`)
 });
 tesla.logData()
