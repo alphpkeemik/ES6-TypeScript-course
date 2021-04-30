96e74dd174d13ebd2ad37baaf3037ed2fcc2f771
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 13:34:26 +0300

    promised foo

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+const mockRequest = new Promise((resolve, reject) => {
+    resolve ('fuu')
+})
 
-const orderedFavoriteFruits = ['watermelon', 'avocado', 'cherries', 'bananas', 'apples', 'strawberries', 'avocado', 'apples']
-const dedupe = array => Array.from(new Set(array))
-console.log([...new Set(orderedFavoriteFruits)])
-
-console.log(dedupe(orderedFavoriteFruits))
+console.log(mockRequest)
