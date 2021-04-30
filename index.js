7b5da0e5ac1cee2003ef4d6c6eed56866530e0cc
Mati Andreas <mati.andreas@ambientia.ee>
2021-04-30 14:10:26 +0300

    promise all

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ const mockRequestShort = () => new Promise((resolve, reject) => {
     setTimeout(() => {
         //reject(new Error('Oh no, something went wrong'))
         console.log('short')
-        resolve('foo')
+        resolve('short-r')
     }, 500)
 });
 const mockRequestMedium = () => new Promise((resolve, reject) => {
     setTimeout(() => {
         //reject(new Error('Oh no, something went wrong'))
         console.log('medium')
-        resolve('foo')
+        resolve('medium-r')
     }, 1000)
 });
 const mockRequestLong = () => new Promise((resolve, reject) => {
     setTimeout(() => {
         //reject(new Error('Oh no, something went wrong'))
         console.log('long')
-        resolve('foo')
+        resolve('long-r')
     }, 1500)
 
 });
 
 (async () => {
-    await mockRequestMedium()
-    await mockRequestShort()
-
+    const [medium, long, short] = await Promise.all([
+        mockRequestMedium(),
+        mockRequestLong(),
+        mockRequestShort()
+    ])
+    console.log(medium, long, short)
+    console.log('all')
 
 })()
